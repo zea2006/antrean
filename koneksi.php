4c101ca33da4b3739377401925e16d393989b64f
zea2006 <zea2006@gmail.com>
2025-06-30 10:22:23 +0800

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set("Asia/Kuala_Lumpur");
 $host = "localhost";
 $user = "root";
 $pass = "";
@@ -9,4 +9,3 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-?>
